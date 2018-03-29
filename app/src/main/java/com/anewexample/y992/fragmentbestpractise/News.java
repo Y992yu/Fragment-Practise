9dfe6d6c0a0e65844a89bcdc6e913fb49d82e4be
992 <yycayt@sina.com>
2018-03-29 19:16:03 +0800

    practise

---
+++ app/src/main/java/com/anewexample/y992/fragmentbestpractise/News.java
@@ -0,0 +1,29 @@
+package com.anewexample.y992.fragmentbestpractise;
+
+/**
+ * Created by y992 on 2018/3/23.
+ */
+//新建新闻实体类
+public class News {
+
+    private String title;
+
+    private String content;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/anewexample/y992/fragmentbestpractise/NewsContentFragment.java
@@ -0,0 +1,36 @@
+package com.anewexample.y992.fragmentbestpractise;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by y992 on 2018/3/23.
+ */
+
+public class NewsContentFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_content_frag,container,false);
+        return view;
+    }//加载创建的布局
+
+    //将新闻的标题和内容显示到界面上
+    public void refresh(String newsTitle, String newsContent)
+    {
+        View visibilityLayout = view.findViewById(R.id.visibility_layout);
+        visibilityLayout.setVisibility(View.VISIBLE);
+        TextView newsTitleText = (TextView) view.findViewById(R.id.news_title);
+        TextView newsContentText = (TextView) view.findViewById(R.id.news_content);
+        newsTitleText.setText(newsTitle);//刷新新闻标题
+        newsContentText.setText(newsContent);//刷新新闻内容
+    }
+}
